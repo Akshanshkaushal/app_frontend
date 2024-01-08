9c92f2ae9bb7a5cd91f4983ac6e1a9285b7b978f
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-01-08 22:45:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Stories from './components/Stories';
+import Post from './components/Post';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+     <>
+      <Stories/>
+      <Post/>
+      <Navbar/>
+     </>
+  );
+}
+
+export default App;
