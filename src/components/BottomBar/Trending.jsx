24b887113adaba0c3cea786fdc70ea0976bc1826
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-01-22 01:56:02 +0530

    trending

--- src/components/BottomBar/Trending.jsx
+++ src/components/BottomBar/Trending.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { useNavigate, Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { dumy } from '../../assets';
+import "./Trending.css";
 
 export default function Trending() {
   const [movies, setMovies] = useState([]);
@@ -40,47 +45,91 @@ export default function Trending() {
     return <div>{error}</div>;
   }
 
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,  // Hide arrows
+  };
+
+
   return (
     <div className="mb-4">
-      <div className='flex flex-row justify-center mb-4 items-center bg-gray-800 w-full h-[4rem]'>
-        <IoIosArrowBack size={20} className='text-white absolute left-0 mx-2' onClick={() => navigate("/home")} />
-        <h3 className="text-3xl font-bold text-white ">Trending</h3>
-      </div>
-      <div className='flex flex-row justify-center text-2xl mb-6 items-center gap-6 w-full'>
-        <button
-          className={`flex justify-start items-center ${activeTab === 'movies' ? 'text-blue-500' : 'text-white'}`}
-          onClick={() => setActiveTab('movies')}
-        >
-          Movies
-        </button>
-        <button
-          className={` ${activeTab === 'tv' ? 'text-blue-500' : 'text-white'}`}
-          onClick={() => setActiveTab('tv')}
-        >
-          TV Shows
-        </button>
-      </div>
+      <div className="flex flex-col overflow-x-auto gap-0 scrollb  ">
 
-      <div className="grid grid-cols-2 gap-2 mx-2">
-        {activeTab === 'movies' &&
-          movies.map((item) => (
-            <Link to={`/details/movie/${item.id}`} key={item.id}>
-              <div className="movie-card">
-                <img src={item.image_url} alt={item.title} />
-                <h3 className='text-white'>{item.title}</h3>
-              </div>
-            </Link>
-          ))}
-        {activeTab === 'tv' &&
-          tvShows.map((item) => (
-            <Link to={`/details/tv/${item.id}`} key={item.id}>
-              <div className="movie-card">
-                <img src={item.image_url} alt={item.name} />
-                <h3>{item.name}</h3>
-              </div>
-            </Link>
-          ))}
+      <Slider {...sliderSettings} className='m-4'>
+ 
+        <div className='h-56'>
+        <div className='relative'>
+        <img src={dumy} alt="Slider Image 1" className="w-full h-48 object-cover rounded-lg" />
+          <div className="text-gray-200 absolute bottom-20 text-xl mx-2">Heading</div>
+          <div className="text-gray-300 absolute bottom-12 text-md mx-2">Text</div>
+          <div className='bg-green-500 flex flex-row absolute top-0 w-full rounded-lg'>
+         <div className='bg-black px-2 m-1 text-white mx-2 rounded-md'>TV</div>
+         <div className='ml-auto mx-2 text-white m-1' >Subscribe To Watch</div>
+         <div className='bg-black px-2 m-1 text-white mr-2 rounded-md'>tv</div>
+        </div>
+          </div>
+          <div className='bg-gray-600 gap-2 flex flex-row absolute bottom-3 mx-4 opacity-80 rounded-md p-1 pl-2 pr-16 text-white'>
+          <div className='bg-black px-1 '>tv+</div>
+          <div className='text-xl'>Play on Apple TV+</div>
+        </div>
+    
+        </div>
+     
+        <div>
+        </div>
+    
+      </Slider>
+
+
+      <div className='flex flex-row text-gray-200 gap-2 mx-4 mb-2'> 
+      <div>popular tv shows</div>
+      <IoIosArrowForward size={20} className='text-white mx-2' onClick={() => navigate("/home")} />
+            </div>
+      <div className='flex flex-row overflow-x-auto overflow-y-hidden scrollb'> 
+  {movies.map((item) => (
+    <Link to={`/details/movie/${item.id}`} key={item.id}>
+      <div className="relative movie-card-container h-64 w-40  flex flex-col justify-between ">
+        <div className="movie-card ">
+          <img src={item.image_url} alt={item.title} className='w-full h-3/4 object-contain mb-4 rounded-4xl ' />
+        </div>
+        <h3 className='absolute bottom-8 left-4  text-gray-200 text-md'>{item.title}</h3>
+
       </div>
+    </Link>
+    
+  ))}
+</div>
+
+
+<div className='flex flex-row text-gray-200 gap-2 mx-4 mb-2'> 
+      <div>popular tv shows</div>
+      <IoIosArrowForward size={20} className='text-white mx-2 ' onClick={() => navigate("/home")} />
+            </div>
+      <div className='flex flex-row overflow-x-auto overflow-y-hidden scrollb'> 
+  {movies.map((item) => (
+    <Link to={`/details/movie/${item.id}`} key={item.id}>
+      <div className="relative movie-card-container h-64 w-40  flex flex-col justify-between ">
+        <div className="movie-card ">
+          <img src={item.image_url} alt={item.title} className='w-full h-3/4 object-contain mb-4 rounded-4xl ' />
+        </div>
+        <h3 className='absolute bottom-8 left-4  text-gray-200 text-md'>{item.title}</h3>
+
+      </div>
+    </Link>
+    
+  ))}
+</div>
+
+
+
+
+
+
+</div>  
     </div>
   );
 }
