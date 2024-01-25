4a704f6f4b25187f040939f1a20cbc252fad672f
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-01-25 19:29:52 +0530

    search_updated

--- src/components/PlaylistSection/PlayList/PlayListDetails.jsx
+++ src/components/PlaylistSection/PlayList/PlayListDetails.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { IoIosArrowBack, IoIosArrowDown } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
-import { dumy } from '../../../assets';
+import { dumy, postimg1, postimg2, postimg3, postimg5, postimg6, postimg7 } from '../../../assets';
 import { FaCross, FaCut, FaFilter, FaHome, FaTicketAlt } from 'react-icons/fa';
+import { TiTick } from "react-icons/ti";
 
 const MovieCard = ({ movie, id }) => {
   const navigate = useNavigate();
@@ -33,13 +34,13 @@ const TrackCard = ({ track, id }) => {
       className=" shadow-md rounded-md h-[13rem] flex flex-row justify-center items-center px-2"
       onClick={handleClick}
     >
-      <div className='' style={{ width: "40%" }}><img src={dumy} className='h-[12.8rem] w-full object-cover' /></div>
+      <div className='' style={{ width: "40%" }}><img src={postimg2} className='h-[12.8rem] w-full object-cover' /></div>
       <div style={{ width: "60%" }} className='flex flex-col  h-full gap-2 px-2 '>
         <div className='text-lg text-gray-300'>{track.title}</div>
         <div className='flex flex-row items-center gap-2 mt-8'>
           <div>S1 E{track.episode}</div>
           <div>+2</div>
-          <FaTicketAlt className='ml-auto' />
+          <TiTick  className='ml-auto'/>
         </div>
         <div className='text-sm text-gray-400 mt-2'>The Breaking Point</div>
         <div className='bg-gray-500 text-white p-1 px-4 mt-auto w-3/4 rounded-lg mb-2 mx-2'><button className='w-full flex flex-row justify-center items-center gap-2'>
@@ -51,18 +52,101 @@ const TrackCard = ({ track, id }) => {
   );
 };
 
+
+const movieData = [
+   { id: 1, title: "Movie 1", episode: 3, status: 'continueWatching', image:  postimg1 },
+  { id: 2, title: "Movie 2", episode: 4, status: 'continueWatching', image:  postimg2 },
+  { id: 3, title: "Movie 3", episode: 2, status: 'continueWatching', image:  postimg3 },
+ 
+   { id: 11, title: "Movie 11", episode: 1, status: "Havent started", image:  postimg1 },
+  { id: 12, title: "Movie 12", episode: 5, status: "Havent started", image:  postimg2 },
+  { id: 13, title: "Movie 13", episode: 3, status: "Havent started", image:  postimg3 },
+ 
+   { id: 21, title: "Movie 21", episode: 1, status: 'Comming soon', image:  postimg2 },
+  { id: 22, title: "Movie 22", episode: 6, status: 'Comming soon', image:  postimg5 },
+  { id: 23, title: "Movie 23", episode: 4, status: 'Comming soon', image:  postimg7 },
+ 
+   { id: 4, title: "Movie 4", episode: 2, status: 'continueWatching', image:  postimg5 },
+  { id: 5, title: "Movie 5", episode: 3, status: 'continueWatching', image:  postimg6 },
+ 
+ 
+  { id: 14, title: "Movie 14", episode: 4, status: "Havent started", image:  postimg1 },
+  { id: 15, title: "Movie 15", episode: 2, status: "Havent started", image:  postimg2 },
+ 
+  
+  { id: 24, title: "Movie 24", episode: 5, status: 'Comming soon', image:  postimg7 },
+  { id: 25, title: "Movie 25", episode: 3, status: 'Comming soon', image:  postimg5 },
+   
+];
+
+movieData.forEach((movie, index) => {
+  movie.value = (index + 1) * 5; // You can adjust this formula as needed
+});
+
+const MovieCardstatic = ({ movie, id }) => {
+  return (
+    <div className='flex flex-col justify-center'>
+    <div
+      className="shadow-md rounded-md h-[13rem] flex flex-row justify-center items-center px-2"
+     
+    >
+      <div style={{ width: "40%" }}>
+        <img src={movie.image} className='h-[12.8rem] w-full object-cover' alt={`Movie ${id}`} />
+      </div>
+      <div style={{ width: "60%" }} className='flex flex-col h-full gap-2 px-2 '>
+        <div className='text-lg text-gray-300'>{movie.title}</div>
+        <div className='flex flex-row items-center gap-2 mt-8'>
+          <div>S1 E{movie.episode}</div>
+          <div>+2</div>
+          <TiTick  className='ml-auto'/>
+        </div>
+        <div className='text-sm text-gray-400 mt-2'>{movie.status}</div>
+        <div className='bg-gray-500 text-white p-1 px-4 mt-auto w-3/4 rounded-lg mb-2 mx-2'>
+          <button className='w-full flex flex-row justify-center items-center gap-2'>
+            <FaHome />
+            <div className='text-xl'>Watch Now</div>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="h-2 bg-green-500" style={{ width: `${movie?.value}%` }}></div>
+</div>
+  );
+};
+
+
+
+const renderMovies = ({activeTab}) => {
+  console.log("render")
+  console.log(activeTab)
+  const filteredMoviesstatic = movieData.filter(movie => movie.status === activeTab);
+  console.log(filteredMoviesstatic)
+  return (
+    <div className="grid grid-rows-1 gap-2">
+      {filteredMoviesstatic.map(movie => (
+        <MovieCardstatic key={movie.id} movie={movie} id={movie.id} />
+      ))}
+    </div>
+  );
+};
+
 const PlaylistDetails = ({ playlistData, type, trackedData }) => {
+ console.log(trackedData)
   const navigate = useNavigate();
-  const [activeTab, setActiveTab] = useState('movie');
+  const [activeTab, setActiveTab] = useState('tv');
+
+   
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
 
-  
-  const filteredData = playlistData?.movies.filter((item) => item.content_type === activeTab);
- console.log(trackedData)
 
+  const [value, setValue] = useState("50");
+
+  const filteredData = playlistData?.movies.filter((item) => item.content_type === activeTab);
+ 
+console.log(activeTab)
   return (
     <div className="text-white overflow-x-hidden  shadow-md rounded-md">
       <div className='flex flex-row mb-4  items-center bg-gray-800 w-full h-[4rem]'>
@@ -91,11 +175,18 @@ const PlaylistDetails = ({ playlistData, type, trackedData }) => {
       </div>
 
       {type === 'Tracking'  && (
-      <div className='flex flex-row justify-center items-center gap-2 m-2 my-4'>
-        <div className='bg-gray-400 text-black p-1'>continue watching</div>
-        <div className='bg-gray-400 text-black p-1'>continue watching</div>
-        <div className='bg-gray-400 text-black p-1'>continue watching</div>
-      </div>
+        <div className='flex flex-row justify-center items-center gap-4 '>
+     
+          <div  onClick={() => handleTabChange('continueWatching')}
+        className={`bg-gray-700 px-1 text-white cursor-pointer ${activeTab === 'continueWatching' ? 'bg-gray-700' : ''}`}>continueWatching</div>
+      
+      <div  onClick={() => handleTabChange('Havent started')}
+        className={`bg-gray-700 px-1 text-white  cursor-pointer ${activeTab === 'continueWatching' ? 'bg-gray-700' : ''}`}>Havent started</div>
+
+<div  onClick={() => handleTabChange('Comming soon')}
+        className={`bg-gray-700 px-1 text-white  cursor-pointer ${activeTab === 'continueWatching' ? 'bg-gray-700' : ''}`}>Comming soon</div>
+
+        </div>
       )}
 
       <div className='flex flex-row justify-center items-center gap-4 m-2 mt-4'>
@@ -109,8 +200,12 @@ const PlaylistDetails = ({ playlistData, type, trackedData }) => {
        
       </div>
 
-      <div className='bg-gray-300 w-full mb-4 opacity-25 mx-4 h-[0.1rem]'></div>
+  <div className='bg-gray-300 w-full mb-4 opacity-25 mx-4 h-[0.1rem]'></div>
 
+
+  {activeTab === 'continueWatching' && renderMovies({ activeTab })}
+{activeTab === "Havent started" && renderMovies({ activeTab })}
+{activeTab === 'Comming soon' && renderMovies({ activeTab })}
 
       {/* <div className="grid grid-cols-2 gap-2">
         {filteredData.map((item, index) => (
@@ -151,11 +246,10 @@ const PlaylistDetails = ({ playlistData, type, trackedData }) => {
             trackedData.map((track, index) => (
               <div>
               <TrackCard key={index} track={track} id={track.content_id} />
-              <div className="h-2 bg-green-500 w-1/2 mx-2"></div>
-</div>
+              <div className="h-2 bg-green-500" style={{ width: `${value}%` }}></div>
+              </div>
             ))}
-        </div>
-        
+        </div>      
       )}
 
     </div>
